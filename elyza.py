9e1c9771e84d7dcc0390b008cca084e1d89440dd
Daiichiro <noda724@gmail.com>
2023-10-19 06:50:46 +0900

    fix requirements.txt

--- elyza.py
+++ elyza.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 B_INST, E_INST = "[INST]", "[/INST]"
 B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 DEFAULT_SYSTEM_PROMPT = "あなたは誠実で優秀な日本人のアシスタントです。"
-text = "クマが海辺に行ってアザラシと友達になり、最終的には家に帰るというプロットの短編小説を書いてください。"
+text = "plotly dashでグラフを作成し、それをダッシュボードに表示する方法を教えてください。"
 
 model_name = "elyza/ELYZA-japanese-Llama-2-7b-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -43,7 +43,7 @@ with torch.no_grad():
 
     output_ids = model.generate(
         token_ids.to(model.device),
-        max_new_tokens=256,
+        max_new_tokens=1024,
         pad_token_id=tokenizer.pad_token_id,
         eos_token_id=tokenizer.eos_token_id,
     )
@@ -51,17 +51,3 @@ output = tokenizer.decode(
     output_ids.tolist()[0][token_ids.size(1) :], skip_special_tokens=True
 )
 print(output)
-"""
-承知しました。以下にクマが海辺に行ってアザラシと友達になり、最終的には家に帰るというプロットの短編小説を記述します。
-
-クマは山の中でゆっくりと眠っていた。
-その眠りに落ちたクマは、夢の中で海辺を歩いていた。
-そこにはアザラシがいた。
-クマはアザラシに話しかける。
-
-「おはよう」とクマが言うと、アザラシは驚いたように顔を上げた。
-「あ、こんにちは」アザラシは答えた。
-クマはアザラシと友達になりたいと思う。
-
-「私はクマと申します。」クマは...
-"""
